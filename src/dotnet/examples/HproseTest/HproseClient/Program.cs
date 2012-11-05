c263c847b8872033fc84d164e972b0ebe758e7c4
andot <mabingyao@gmail.com>
2012-11-06 02:04:25 +0800

    Added a example of Hprose for .NET

---
+++ src/dotnet/examples/HproseTest/HproseClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hprose.Client;
+using Hprose.IO;
+
+namespace HproseClient
+{
+    [Serializable]
+    public class User
+    {
+        public string name;
+        public int age;
+        public bool male;
+        public List<User> friends;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassManager.Register(typeof(User), "User");
+            HproseHttpClient client = new HproseHttpClient("http://localhost:2012/");
+            List<User> users = new List<User>();
+            User user1 = new User();
+            user1.name = "李雷";
+            user1.age = 32;
+            user1.male = true;
+            user1.friends = new List<User>();
+            User user2 = new User();
+            user2.name = "韩梅梅";
+            user2.age = 31;
+            user2.male = false;
+            user2.friends = new List<User>();
+            user1.friends.Add(user2);
+            user2.friends.Add(user1);
+            users.Add(user1);
+            users.Add(user2);
+            MemoryStream stream = (MemoryStream)client.Invoke("sendUsers", new object[] { users }, Hprose.Common.HproseResultMode.RawWithEndTag);
+            Console.WriteLine(UTF8Encoding.Default.GetString(stream.ToArray()));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/dotnet/examples/HproseTest/HproseServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hprose.Server;
+using Hprose.IO;
+
+namespace HproseServer
+{
+    [Serializable]
+    public class User
+    {
+        public string name;
+        public int age;
+        public bool male;
+        public List<User> friends;
+    }
+    class TestService
+    {
+        public List<User> SendUsers(List<User> users)
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine("name={0}, age={1}, male={2}", user.name, user.age, user.male);
+            }
+            return users;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassManager.Register(typeof(User), "User");
+            HproseHttpListenerServer server = new HproseHttpListenerServer("http://localhost:2012/");
+            server.Methods.AddInstanceMethods(new TestService());
+            server.IsCrossDomainEnabled = true;
+            server.CrossDomainXmlFile = "crossdomain.xml";
+            server.Start();
+            Console.WriteLine("Server started.");
+            Console.ReadLine();
+            Console.WriteLine("Server stopped.");
+        }
+    }
+}
