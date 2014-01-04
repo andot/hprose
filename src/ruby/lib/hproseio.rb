b33cecc9dafdb980354c9cb66961336a72d355d2
andot <mabingyao@gmail.com>
2014-01-04 15:22:24 +0800

    Update hprose for Ruby

--- src/ruby/lib/hproseio.rb
+++ src/ruby/lib/hproseio.rb
@@ -14,7 +14,7 @@
 #                                                          #
 # hprose io for ruby                                       #
 #                                                          #
-# LastModified: Dec 1, 2012                                #
+# LastModified: Jan 4, 2014                                #
 # Author: Ma Bingyao <andot@hprfc.com>                     #
 #                                                          #
 ############################################################
@@ -22,13 +22,19 @@
 module Hprose
   autoload :Tags, 'hprose/io'
   autoload :ClassManager, 'hprose/io'
+  autoload :RawReader, 'hprose/io'
+  autoload :SimpleReader, 'hprose/io'
   autoload :Reader, 'hprose/io'
+  autoload :SimpleWriter, 'hprose/io'
   autoload :Writer, 'hprose/io'
   autoload :Formatter, 'hprose/io'
 end
 
 Object.const_set(:HproseTags, Hprose.const_get(:Tags))
 Object.const_set(:HproseClassManager, Hprose.const_get(:ClassManager))
+Object.const_set(:HproseRawReader, Hprose.const_get(:RawReader))
+Object.const_set(:HproseSimpleReader, Hprose.const_get(:SimpleReader))
 Object.const_set(:HproseReader, Hprose.const_get(:Reader))
+Object.const_set(:HproseSimpleWriter, Hprose.const_get(:SimpleWriter))
 Object.const_set(:HproseWriter, Hprose.const_get(:Writer))
 Object.const_set(:HproseFormatter, Hprose.const_get(:Formatter))
